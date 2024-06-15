142b135f4c38d7c3d64996271cbb70324a72ab7a
OmarAlrifaee <jivara211@gmail.com>
2024-06-15 16:30:31 +0300

    add error pages

---
+++ src/app/(auth)/signup/error.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import AddNewLink from "@/components/AddNewLink";
+
+type Props = {
+  error: Error;
+  reset: () => void;
+};
+const error = ({ error, reset }: Props) => {
+  return (
+    <section className="p-10 min-h-screen bg-main-bg flex items-center justify-center">
+      <div className="h-1/2 w-1/2 rounded-md bg-main-soft-bg p-10 flex flex-col gap-10">
+        <h3 className="text-white font-bold capitalize text-5xl text-center">
+          User Already Exist
+        </h3>
+        <div className="flex items-center gap-5 justify-center">
+          <button
+            onClick={reset}
+            className="font-bold text-white bg-red-500 rounded-md px-5 py-2 transition hover:bg-red-200"
+          >
+            Try Agine
+          </button>
+          <AddNewLink href="/signup" text="Go To signup" />
+        </div>
+      </div>
+    </section>
+  );
+};
+export default error;
