89056bf0356a1fb9de24ea8006b7bb526637e736
OmarAlrifaee <jivara211@gmail.com>
2024-06-15 15:56:45 +0300

    add loading pages to all of the pages

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -3,7 +3,7 @@ const Loader = ({}: Props) => {
   return (
     <div className="flex items-center justify-center">
       <div
-        className={`border-y-2 border-y-black animate-spin rounded-full size-5`}
+        className={`border-y-2 border-y-white animate-spin rounded-full size-5`}
       />
     </div>
   );
---
+++ src/components/PagesLoader.tsx
@@ -0,0 +1,10 @@
+const PagesLoader = () => {
+  return (
+    <div className="flex items-center justify-center mt-20">
+      <div
+        className={`border-t-4 border-t-white animate-spin rounded-full size-20`}
+      />
+    </div>
+  );
+};
+export default PagesLoader;
