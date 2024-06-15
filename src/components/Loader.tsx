ffbb1b76ed8fb48316a1cc753986a3644d7f79a7
OmarAlrifaee <jivara211@gmail.com>
2024-06-15 16:15:47 +0300

    fix loader component

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -1,11 +1,10 @@
 type Props = {};
 const Loader = ({}: Props) => {
   return (
-    <div className="flex items-center justify-center">
-      <div
-        className={`border-y-2 border-y-white animate-spin rounded-full size-5`}
-      />
-    </div>
+    <div
+      className="inline-block h-4 w-4 animate-spin rounded-full border-2 border-solid border-current border-e-transparent align-[-0.125em] text-surface motion-reduce:animate-[spin_1.5s_linear_infinite] dark:text-white"
+      role="status"
+    />
   );
 };
 export default Loader;
